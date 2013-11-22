8e0dd1829500d51c5a72cfd5124545786799c8d3
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-22 06:43:41 -0800

    Create payload with `new_payload`. Update endpoint to github-commit. Make job name configurable via config.

--- lib/fission-rest-api/api/github.rb
+++ lib/fission-rest-api/api/github.rb
@@ -1,23 +1,21 @@
 require 'multi_json'
+require 'fission/utils'
 
 Carnivore::PointBuilder.define do
 
-  post %r{/github-build/?}, :workers => Carnivore::Config.get(:fission, :workers, :git_build) || 1 do |msg|
+  post %r{/github-commit/?}, :workers => Carnivore::Config.get(:fission, :workers, :git_build) || 1 do |msg|
     begin
-      payload = symbolize_hash(MultiJson.load(msg[:message][:body].to_s))
-      payload = {
-        :job => 'package_builder',
-        :message_id => Celluloid.uuid,
-        :github => payload
-      }
+      job_name = Carnivore::Config.get(:fission, :rest_api, :github_commit, :job_name) || :router
+      payload = MultiJson.load(msg[:message][:body])
+      payload = Fission::Utils.new_payload(job_name, :github => payload)
       debug "Processing payload: #{payload}"
-      Fission::Utils.transmit(:fission_package_builder, payload)
+      Fission::Utils.transmit(job_name, payload)
       msg.confirm!(:response_body => 'Job submitted for build')
     rescue MultiJson::DecodeError
       error 'Failed to parse JSON from request'
       msg.confirm!(:response_body => 'Invalid JSON data', :code => :bad_request)
     rescue => e
-      error "Unknown error: #{e.class}: #{e}"
+      error "Unknown error: #{e.class}: #{e.message}"
       debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
       msg.confirm!(:response_body => 'Unexpected error encountered', :code => :internal_server_error)
     end
