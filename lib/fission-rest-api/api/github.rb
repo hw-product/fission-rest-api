74d0c8791aefa0ca9b9b494f2b4152e2f6c1f24c
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-06 08:11:10 -0800

    Confirm message and bail

--- lib/fission-rest-api/api/github.rb
+++ lib/fission-rest-api/api/github.rb
@@ -3,15 +3,16 @@ require 'fission/utils'
 
 Carnivore::PointBuilder.define do
 
-  post %r{/github-commit/?}, :workers => Carnivore::Config.get(:fission, :workers, :github_commit) || 1 do |msg|
+  post %r{/github-commit/?}, :workers => Carnivore::Config.get(:fission, :workers, :github_commit) || 1 do |msg, *args|
     begin
       job_name = Carnivore::Config.get(:fission, :rest_api, :github_commit, :job_name) || :router
       payload = MultiJson.load(msg[:message][:query][:payload])
-      if(filter = msg[:message][:query][:pkgr_filter])
+      if(filter = msg[:message][:query][:filter])
         debug "Detected pkgr filter on #{msg} - #{filter}"
         unless(File.join('refs/head', filter) == payload['ref'])
           warn "Discarding #{msg} due to filter match failure. filter: #{filter} ref: #{payload['ref']}"
           msg.confirm!(:response_body => 'Job discared due to filter')
+          return # short circuit
         end
       end
       payload = Fission::Utils.new_payload(job_name, :github => payload)
