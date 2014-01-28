1c8ff52b0667c8d4e351aa8efcb0b9430d187413
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-28 13:36:10 -0800

    Allow github API to handle tag and branch based filtering

--- lib/fission-rest-api/api/github.rb
+++ lib/fission-rest-api/api/github.rb
@@ -10,39 +10,44 @@ Carnivore::PointBuilder.define do
       end
       job_name = Carnivore::Config.get(:fission, :rest_api, :github_commit, :job_name) || :router
       payload = MultiJson.load(msg[:message][:query][:payload] || msg[:message][:body])
+      valid = []
       if(filter = msg[:message][:query][:filter])
         debug "Detected pkgr filter on #{msg} - #{filter}"
-        unless(File.join('refs/heads', filter) == payload['ref'])
-          warn "Discarding #{msg} due to filter match failure. filter: #{filter} ref: #{payload['ref']}"
-          msg.confirm!(:response_body => 'Job discared due to filter')
-          return # short circuit
-        end
-      elsif(msg[:message][:query][:tags])
-        if(payload['ref'].start_with?('refs/tags'))
-          if(payload['deleted'] == true)
-            warn "Discarding #{msg} due to tag destruction type event (tag builds enabled). ref: #{payload['ref']}"
-            msg.confirm!(:response_body => 'Job discarded due to non-create type tag event')
-            return # short circuit
-          end
+        if(File.join('refs/heads', filter) == payload['ref'])
+          valid << true
         else
-          warn "Discarding #{msg} due to non tag type event (tag builds enabled). ref: #{payload['ref']}"
-          msg.confirm!(:response_body => 'Job discarded due to non-tag type event')
-          return # short circuit
-        end
-      else
-        # detect this is a commit push type event
-        unless(payload['ref'].start_with?('refs/heads'))
-          warn "Discarding #{msg} due to non commit type event. ref: #{payload['ref']}"
-          msg.confirm!(:response_body => 'Job discarded due to non-commit type event')
-          return # short circuit
+          warn "Filter match failure on #{msg}. filter: #{filter} ref: #{payload['ref']}"
         end
       end
-      payload = Fission::Utils.new_payload(job_name, :github => payload)
-      payload[:data][:github_status] = {:state => :pending}
-      payload[:data][:router] = {:action => action}
-      debug "Processing payload: #{payload}"
-      Fission::Utils.transmit(job_name, payload)
-      msg.confirm!(:response_body => 'Job submitted for processing!')
+      if(msg[:message][:query][:tags])
+        if(payload['ref'].start_with?('refs/tags'))
+          if(payload['deleted'] == true)
+            warn "Tag match failure #{msg} due to tag destruction type event (tag builds enabled). ref: #{payload['ref']}"
+          else
+            valid << true
+          end
+        else
+          warn "Tag match failure #{msg} due to non tag type event (tag builds enabled). ref: #{payload['ref']}"
+        end
+      end
+      if(valid.empty?)
+        # detect this is a commit push type event
+        unless(payload['ref'].start_with?('refs/heads'))
+          warn "Commit match failure on #{msg} due to non commit type event. ref: #{payload['ref']}"
+        else
+          valid << true
+        end
+      end
+      if(valid.include?(true))
+        payload = Fission::Utils.new_payload(job_name, :github => payload)
+        payload[:data][:github_status] = {:state => :pending}
+        payload[:data][:router] = {:action => action}
+        debug "Processing payload: #{payload}"
+        Fission::Utils.transmit(job_name, payload)
+        msg.confirm!(:response_body => 'Job submitted for processing!')
+      else
+        msg.confirm!(:response_body => 'Job discarded due to filter')
+      end
     rescue MultiJson::DecodeError
       error "Failed to parse JSON from request (#{msg})"
       debug "M: #{msg[:message].inspect}"
