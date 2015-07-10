7182f7179f6db8bb1899f37c56834ec48bec3d25
Chris Roberts <code@chrisroberts.org>
2015-07-10 13:33:44 -0700

    Provide loaders

--- lib/fission-rest-api.rb
+++ lib/fission-rest-api.rb
@@ -1,2 +1,11 @@
+require 'fission'
 require 'fission-rest-api/version'
+
+module Fission
+  module RestApi
+    autoload :Repository, 'fission-rest-api/repository'
+    autoload :Helpers, 'fission-rest-api/helpers'
+  end
+end
+
 require 'fission-rest-api/repository'
